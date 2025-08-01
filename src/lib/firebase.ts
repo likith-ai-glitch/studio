71a696998733e0999ed2d690506f936b2e55e2a6
Likith Gowda <likithknml@gmail.com>
2025-08-01 18:11:37 +0000

    no im getting the same

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // This is a public configuration and is safe to be exposed here.
 const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: "shopstream-2vvbc",
+  appId: "1:637976165114:web:6c6366c6e1bc5e33b52743",
+  storageBucket: "shopstream-2vvbc.firebasestorage.app",
+  apiKey: "AIzaSyDHi9PXw33gXSMrhUS_dF9bGyWS_yKzMDI",
+  authDomain: "shopstream-2vvbc.firebaseapp.com",
+  messagingSenderId: "637976165114",
 };
 
 
