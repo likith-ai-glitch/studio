fd017a32ba4bd7b62dc67dcd3c0c5f2b5446a5ed
Likith Gowda <likithknml@gmail.com>
2025-08-01 18:44:46 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwC_t52_iI3-CwhpTOrpG3zEtrigRT9hM",
-  authDomain: "shopstream-2vvbc.firebaseapp.com",
-  projectId: "shopstream-2vvbc",
-  storageBucket: "shopstream-2vvbc.appspot.com",
-  messagingSenderId: "637976165114",
-  appId: "1:637976165114:web:6c6366c6e1bc5e33b52743",
-  measurementId: "G-RFR5PZ39GC"
+  "projectId": "shopstream-2vvbc",
+  "appId": "1:637976165114:web:6c6366c6e1bc5e33b52743",
+  "storageBucket": "shopstream-2vvbc.firebasestorage.app",
+  "apiKey": "AIzaSyDHi9PXw33gXSMrhUS_dF9bGyWS_yKzMDI",
+  "authDomain": "shopstream-2vvbc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "637976165114"
 };
 
 
