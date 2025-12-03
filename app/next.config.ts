0a8be30df602ed69ceffc4a0cb3a59646cadb6f5
Likith Gowda <likithknml@gmail.com>
2025-12-03 04:39:28 +0000

    #!/bin/bash set -euo pipefail
    echo "1) Show listeners on 9010..9030 (if

---
+++ app/next.config.ts
@@ -0,0 +1,27 @@
+
+import { config } from 'dotenv';
+config();
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
