4ac86c6a3f1205029d8d4a1277f812778ad0ebef
Likith Gowda <likithknml@gmail.com>
2025-12-05 07:50:25 +0000

    still 404 error is same

---
+++ app/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "https://9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
+    ]
+  }
+};
+
+module.exports = nextConfig;
