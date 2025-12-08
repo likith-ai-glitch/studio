a6576fe68027753c8403cde6ae438179615f0f83
Likith Gowda <likithknml@gmail.com>
2025-12-08 13:55:55 +0000

    still the error dint get solved

--- usr/local/google/home/user/studio/next-project/next.config.js
+++ usr/local/google/home/user/studio/next-project/next.config.js
@@ -10,9 +10,7 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        "9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
-      ]
+      allowedOrigins: ["9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev", "9015-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
     }
   }
 };
