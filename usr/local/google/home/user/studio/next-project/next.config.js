301f53b7d0f44f3b9a33f0e12122d0780d9218f7
Likith Gowda <likithknml@gmail.com>
2025-12-08 12:57:37 +0000

    solve this error

---
+++ usr/local/google/home/user/studio/next-project/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
+      ]
+    }
+  }
+};
+
+module.exports = nextConfig;
