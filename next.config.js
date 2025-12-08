a52388e603bea09a935dc8f200bd375978e83995
Likith Gowda <likithknml@gmail.com>
2025-12-08 13:54:27 +0000

    in which page im getting 404 error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: [
-      "https://9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
-    ]
+    serverActions: {
+      allowedOrigins: ["9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
+    }
   }
 };
 
