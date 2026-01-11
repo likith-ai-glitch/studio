9294fb4b2e6a2588784f6f75aceaae2a33980e56
Likith Gowda <likithknml@gmail.com>
2026-01-11 16:37:12 +0000

    still getting same error message

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     ],
   },
   // This allows the Next.js dev server to accept requests from the proxied URL in Firebase Studio.
-  allowedDevOrigins: ["https://9015-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
+  allowedDevOrigins: ["https://6000-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
   experimental: {
     serverActions: {
       allowedOrigins: ["9015-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev", "9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
