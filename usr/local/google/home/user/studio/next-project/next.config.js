4b22e6b28cbe8ab24eb0e4cc8547450efed79ba9
Likith Gowda <likithknml@gmail.com>
2026-01-11 16:38:45 +0000

    still getting same error message

--- usr/local/google/home/user/studio/next-project/next.config.js
+++ usr/local/google/home/user/studio/next-project/next.config.js
@@ -8,11 +8,8 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev", "9015-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
-    }
-  }
+  // This allows the Next.js dev server to accept requests from the proxied URL in Firebase Studio.
+  allowedDevOrigins: ["https://6000-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
 };
 
 module.exports = nextConfig;
