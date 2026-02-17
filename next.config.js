e2a2d507f19573f1bef92a5affccbaea3ea987da
Likith Gowda <likithknml@gmail.com>
2026-02-17 10:45:00 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   allowedDevOrigins: ["https://6000-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
   experimental: {
     serverActions: {
-      allowedOrigins: ["6000-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
+      allowedOrigins: ["6000-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev", "9015-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev", "9013-firebase-studio-1754066778009.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"]
     }
   }
 };
