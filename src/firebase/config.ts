43fef496969897ba3e84ddc33bac182cd1b38dc2
Likith Gowda <likithknml@gmail.com>
2026-02-27 11:05:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "shopstream-2vvbc",
+  "appId": "1:637976165114:web:6c6366c6e1bc5e33b52743",
+  "apiKey": "AIzaSyDHi9PXw33gXSMrhUS_dF9bGyWS_yKzMDI",
+  "authDomain": "shopstream-2vvbc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "637976165114"
+};
